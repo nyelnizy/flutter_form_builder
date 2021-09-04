e48fda587edd4200f9212a4999c17fd4d8a44871
yhiamdan <50914733+nyelnizy@users.noreply.github.com>
2021-09-04 22:41:48 +0000

    Update form_builder_dropdown.dart
    commented state.requestFocus()

--- lib/src/fields/form_builder_dropdown.dart
+++ lib/src/fields/form_builder_dropdown.dart
@@ -234,7 +234,7 @@ class FormBuilderDropdown<T> extends FormBuilderField<T> {
             // TextFormField
 
             void changeValue(T? value) {
-              state.requestFocus();
+//               state.requestFocus();
               state.didChange(value);
             }
 
